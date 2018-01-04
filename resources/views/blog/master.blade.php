3002b519bd2e186d6e548b476bc1613bcd192010
deki <dekiakbar@linuxmail.org>
2018-01-04 20:01:56 +0700

    add front-end,semanic,font awesome

---
+++ resources/views/blog/master.blade.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/semantic.min.css') }}">
+</head>
+<body>
+	<div class="ui secondary pointing menu">
+		<a class="item">
+		  Website
+		</a>
+		<a class="item active teal">
+		  Blog
+		</a>
+		<a class="item">
+		  About
+		</a>
+		<div class="right menu">
+			<a class="ui item">
+		    	<i class="large github icon"></i>
+			</a>
+			<a class="ui item">
+		    	<i class="blue large facebook icon"></i>
+			</a>
+			<a class="ui item">
+		    	<i class="teal large twitter icon"></i>
+			</a>
+			<a class="ui item">
+		    	<i class="purple large instagram icon"></i>
+			</a>
+		</div>
+	</div>
+	<div class="ui container">
+		<h2 class="ui header">
+		  <i class="book icon"></i>
+		  <div class="content">
+		    Selamat Membaca
+		    <div class="sub header">Pengetahuan itu gratis</div>
+		  </div>
+		</h2>
+	</div>
+	<div class="ui divider"></div>
+	<div class="main content">
+	    <div class="ui column stackable grid">
+	    	<div class="three wide column">
+			    <div class="ui segment">
+			    	<div class="ui accordion">
+						<div class="title">
+							<i class="dropdown icon"></i>
+							Kategori
+						</div>
+						<div class="content">
+							<p class="transition hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+						</div>
+						<div class="title">
+						  <i class="dropdown icon"></i>
+						  Tag
+						</div>
+						<div class="content">
+						  <p class="transition hidden">There are many breeds of dogs. Each breed varies in size and temperament.</p>
+						</div>
+					</div>
+			    </div>
+	    	</div>
+	    	<div class="ten wide column">
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat.
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	    		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	    	</div>
+	    	<div class="three wide column">
+				<div class="ui segment">
+				  Left Rail Content
+				</div>
+	    	</div>
+	    </div>
+	</div>
+	<footer>
+		<div class="ui segment center aligned">
+			<p>&copy;{!! date('Y') !!} 
+				<a href="https://dekiakbar.com">Dekiakbar.com</a> | Made With :
+				<a href="https://Laravel.com">Laravel <i class="large pink heartbeat icon"></i></a>
+			</p>
+		</div>
+	</footer>
+	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/semantic.min.js') }}"></script>
+	<script type="text/javascript">
+		$('.ui.accordion')
+		  .accordion({
+		    selector: {
+		      trigger: '.title .icon'
+		    }
+		  })
+		;
+	</script>
+</body>
+</html>
---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML>
+<!--
+	template designed by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>{!! setting('site.title') !!}</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="{{ asset('css/main.css') }}" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<noscript><link rel="stylesheet" href="{{ asset('css/noscript.css') }}" /></noscript>
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="logo">
+							<span class="icon fa-laptop"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>{!! setting('site.hi') !!}</h1>
+								<p>{!! setting('site.description') !!}</p>
+							</div>
+						</div>
+						<nav>
+							<ul>
+								<li><a href="#intro">Intro</a></li>
+								<li><a href="#blog">Blog</a></li>
+								<li><a href="#about">About</a></li>
+								<li><a href="#contact">Contact</a></li>
+								<!--<li><a href="#elements">Elements</a></li>-->
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Intro -->
+							<article id="intro">
+								<h2 class="major">{!! setting('site.intro_title') !!}</h2>
+								<span class="image main"><img src="images/pic01.jpg" alt="" /></span>
+								<p>{!! setting('site.intro_desc') !!}
+							</article>
+
+						<!-- Work -->
+							<article id="blog">
+								<h2 class="major">Blog</h2>
+								<span class="image main"><img src="images/pic02.jpg" alt="" /></span>
+								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
+								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+							</article>
+
+						<!-- About -->
+							<article id="about">
+								<h2 class="major">{!! setting('site.about_title') !!}</h2>
+								<span class="image main"><img src="{!! setting('site.about_Image') !!}" alt="" /></span>
+								<p>{!! setting('site.about_desc') !!}</p>
+							</article>
+
+						<!-- Contact -->
+							<article id="contact">
+								<h2 class="major">Contact</h2>
+								<form method="post" action="#">
+									<div class="field half first">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name" />
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input type="text" name="email" id="email" />
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="4"></textarea>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Send Message" class="special" /></li>
+										<li><input type="reset" value="Reset" /></li>
+									</ul>
+								</form>
+							</article>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+									<li>
+										<a href="{!! setting('site.twitter') !!}" class="icon fa-twitter">
+											<span class="label">Twitter</span>
+										</a>
+									</li>
+									<li>
+										<a href="{!! setting('site.fb') !!}" class="icon fa-facebook">
+											<span class="label">Facebook</span>
+										</a>
+									</li>
+									<li>
+										<a href="{!! setting('site.ig') !!}" class="icon fa-instagram">
+											<span class="label">Instagram</span>
+										</a>
+									</li>
+									<li>
+										<a href="{!! setting('site.git') !!}" class="icon fa-github">
+											<span class="label">GitHub</span>
+										</a>
+									</li>
+								</ul>
+						<p class="copyright">&copy;{!! date('Y') !!} 
+							<a href="https://dekiakbar.com">Dekiakbar.com</a> | Design: 
+							<a href="https://html5up.net">HTML5</a> | With :
+							<a href="https://Laravel.com">Laravel</a>.
+						</p>
+					</footer>
+
+			</div>
+
+		<!-- BG -->
+			<div id="bg" style="background-image: url('{{ asset('storage') }}/{!! setting('site.bg') !!}');"></div>
+
+		<!-- Scripts -->
+			<script src="{{ asset('js/jquery.min.js') }}"></script>
+			<script src="{{ asset('js/skel.min.js') }}"></script>
+			<script src="{{ asset('js/util.js') }}"></script>
+			<script src="{{ asset('js/main.js') }}"></script>
+	</body>
+</html>
